d8888c87bb4b8202f63495a29e6bc346cf97a6c6
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-06-20 13:21:10 +0530

    initialize database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from "express";
+import "dotenv/config";
+import app from "./app";
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello theere!");
-});
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
